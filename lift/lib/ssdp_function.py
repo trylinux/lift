d520a7314748d800ac4b9e17621a568ef2a60554
Zachary Wikholm <zwikholm@cari.net>
2016-02-25 14:53:29 -0800

    Added in a simple SSDP scanning class

---
+++ lift/lib/ssdp_function.py
@@ -0,0 +1,15 @@
+from scapy.all import *
+
+class ssdp_scan:
+	def active_scan(self, target):
+  		req = 'M-SEARCH * HTTP/1.1\r\nHost:239.255.255.250:1900\r\nST:upnp:rootdevice\r\nMan:"ssdp:discover"\r\nMX:3\r\n\r\n'
+		ip=IP(dst=target)
+  		udp=UDP(sport=random.randint(49152,65536), dport=1900)
+  		pck = ip/udp/req
+  		try:
+   			rep = sr1(pck, verbose=0)
+   			print rep[Raw].load
+  		except:
+   			print "\n[-] Can't send packet :/\n"
+
+
