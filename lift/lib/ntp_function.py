4bb21411aaeaaaefbc7e515f0f2edb2b8b35abc7
Ruth Fombrun <rfombrun@gmail.com>
2017-03-18 20:27:11 -0400

    adds more comments explaining the monlist command in ntp_function

--- lift/lib/ntp_function.py
+++ lift/lib/ntp_function.py
@@ -14,27 +14,35 @@ logger.setLevel(49)
 
 class NTPscan:
     '''Network Time Protocol (NTP) is used to synchronize the time
-    between client and server. It is a UDP protocol that is run on port 123.
+    between a client and server. It is a UDP protocol that is run on port 123.
 
-    In an NTP reflection attack, the attacker sends a crafted packet
-    which requests a large amount of date send to the host.
-    In this case, the attackers are taking advantage of the monlist command.
-    Monlist is a remote command in older versions of NTP that sends the
-    requester a list of the last 600 hosts who have connected to that server.
-    For attackers the monlist query is a great reconnaissance tool.
-    For a localized NTP server it can help to build a network profile.
+    Monlist (the NTP Monitor List Scanner) is a remote command in older
+    versions of NTP that allows administrators to query the server for traffic
+    counts of connected clients. In response the the "get monlist" request, an
+    unpatched NTP server sends the requester a list of the last 600 hosts
+    who have connected to that server.
     '''
 
     def monlist_scan(self, target):
         '''Simulate the monlist command and return `results`, a boolean
         indicating whether or not an answer was received.
+
+        If the `target` device is an NTP server that accepts the monlist
+        request and that server is unpatched, it will return a lot of packets
+        (each about 500 bytes) instead of returning one small packet.
         '''
+        #TODO figure out the relevant exceptions to add and/or handle
+
+        # `data` is the "get monlist" request
         data = "\x17\x00\x03\x2a" + "\x00" * 4
 
+        # Create an IP layer for the packet, spoofing the target's address
         ip = IP(dst=target)
 
+        # Create a UDP layer for the packet
         udp = UDP(sport=random.randint(49152, 65536), dport=123)
 
+        # Create a Raw layer for the packet
         a = Raw(load=data)
 
         # A layer is a subclass of the Packet class.
@@ -42,8 +50,7 @@ class NTPscan:
         # and will be inherited. A simple layer is compounded by a
         # list of fields that will be either concatenated when assembling the
         # layer or dissected one by one when disassembling a string.
-        # Assemble a packet comprised of the layers IP, UDP and Raw
-        # (in that order).
+        # Assemble the packet comprised of the layers IP, UDP and Raw.
         pck = ip/udp/a
 
         # number of attempts
@@ -57,7 +64,7 @@ class NTPscan:
             # The function returns a couple of packet and answers, and the
             # unanswered packets. The function sr1() is a variant that only
             # return one packet that answered the packet (or the packet set)
-            # sent. Send the single SYN packet `pck` to `target`, the given
+            # sent. Send the single packet `pck` to `target`, the given
             # IP address. Quit after receving a single response.
             rep = sr1(pck, verbose=0, timeout=5)
 
