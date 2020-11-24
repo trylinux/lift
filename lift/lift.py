a412f9877de67025ac1a162e493947bd7fece657
Zach W <zachary.wikholm@sony.com>
2020-11-24 09:56:54 -0800

    updating code

--- lift/lift.py
+++ lift/lift.py
@@ -167,7 +167,7 @@ def main():
                         ntp_monlist_check(str(line).rstrip('\r\n'), verbose)
         except KeyboardInterrupt:
             print("Quitting from first try in ifile")
-            sys.exit(0)
+            sys.exit()
         except Exception as e:
             sys.exit()
             print("error in recurse try", e)
@@ -633,7 +633,7 @@ def getheaders(dest_ip, dport, vbose, info):
     except HTTPError as e:
         server = str(e.info().get('Server'))
         auth_header = (e.headers.get('WWW-Authenticate'))
-        if auth_header is not None and str(server) in "alphapd/2.1.8" and int(e.code) == 401:
+        if auth_header is not None and (str(server) in "alphapd/2.1.8" or str(server) in "Embedthis-Appweb") and int(e.code) == 401:
            auth_header_split = auth_header.split(",")
            auth_header_realm = auth_header_split[0].split("=")
            device_model = str(auth_header_realm[1]).replace("\"","")
