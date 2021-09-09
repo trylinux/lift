8a1fdfcdcfffde9018208ef3682ce5f08acf704a
Zachary  Wikholm <zach@Zacharys-MacBook-Pro.local>
2021-09-09 16:29:31 -0700

    adding setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='lift',
+    version='1.0',
+    packages=['lift', 'lift.lib', 'lift.lib.modules'],
+    url='https://github.com/trylinux/lift',
+    license='Freeeee',
+    author='Zachary Wikholm',
+    author_email='kestrel@trylinux.us',
+    description=''
+)
